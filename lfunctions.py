f47c72415c7df4789a2ddb82ef6aeb1c43b4ec25
Seguridad Vial <Seguridad Vial>
2016-07-19 16:24:45 -0300

    marcooooos

--- lfunctions.py
+++ lfunctions.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-import csv
 import functions
 import recoding
 
@@ -20,6 +19,27 @@ def genVictimsTable(strPathToCsv):
                          "modelo" : case['MODELO_VEHICULO_VICTIMA'], "colectivo" : case['COLECTIVO_VICTIMA'], "interno_colectivo" : case['INTERNO_VICTIMA'], "sexo" : case['SEXO_VICTIMA'],
                          "edad" : case['EDAD_VICTIMA'], "sumario" : case['SRIO_NRO'], "id" : id
                          # "tipo_recod" : recoding.vehicleTypeRecoded(case['TIPO_VEHICULO_VICTIMA']),"franja_edad" : recoding.ageRange(int(case['EDAD_VICTIMA']))
+                         }
+            res2.append(formatted)
+    functions.dictsToCsv(res2)
+
+
+def genAccusedsTable(strPathToCsv):
+    rows = functions.csvToDictList(strPathToCsv)
+    res1 = []
+    res2 = []
+    query = functions.db.query('select max(id) from acusados;')
+    id = int(query.all().__getitem__(0)[0])
+    # create a tuple with the fields of each row to compare and check them to avoid adding duplicate entries
+    for case in rows:
+        cmp = (case['ID'],case['EDAD_ACUSADO'],case['SEXO_ACUSADO'],case['ACUSADO'],case['TIPO_VEHICULO_ACUSADO'],case['MARCA_VEHICULO_ACUSADO'],case['MODELO_VEHICULO_ACUSADO'])
+        if not cmp in res1:
+            id += 1
+            res1.append(cmp)
+            formatted = {"id_hecho" : case['ID'], "rol" : case['ACUSADO'], "tipo" : case['TIPO_VEHICULO_ACUSADO'], "marca" : case['MARCA_VEHICULO_ACUSADO'],
+                         "modelo" : case['MODELO_VEHICULO_ACUSADO'], "colectivo" : case['COLECTIVO_ACUSADO'], "interno_colectivo" : case['INTERNO_ACUSADO'], "sexo" : case['SEXO_ACUSADO'],
+                         "edad" : case['EDAD_ACUSADO'], "id" : id
+                         # "tipo_recod" : recoding.vehicleTypeRecoded(case['TIPO_VEHICULO_ACUSADO']),"franja_edad" : recoding.ageRange(int(case['EDAD_ACUSADO']))
                          }
             res2.append(formatted)
     functions.dictsToCsv(res2)
